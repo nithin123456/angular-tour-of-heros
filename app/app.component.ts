d1cee92fea7e8d0e8f92d1fa439a60525aad47f4
Nithin Ankam <c.nithin.ankam@uptake.com>
2016-09-21 23:00:05 -0500

    add input field, import FormModule and add *.js & *.js.map to .gitignore

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: `
+    template:`
     <h1>{{title}}</h1>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label>{{hero.name}}</div>
-    `
+    <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  `
 })
 export class AppComponent {
     title = 'Tour of Heroes';
