1eeb788b338ad27352bfa10df58aa2e36f613962
Nithin Ankam <c.nithin.ankam@uptake.com>
2016-09-21 23:36:28 -0500

    add css class to style selection & restructure app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,23 @@
 import { Component } from '@angular/core';
+
+export class Hero {
+    id:number;
+    name:string;
+}
+
+const HEROES:Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Celeritas'},
+    {id: 15, name: 'Magneta'},
+    {id: 16, name: 'RubberMan'},
+    {id: 17, name: 'Dynama'},
+    {id: 18, name: 'Dr IQ'},
+    {id: 19, name: 'Magma'},
+    {id: 20, name: 'Tornado'}
+];
+
 @Component({
     selector: 'my-app',
     template: `
@@ -13,7 +32,9 @@ import { Component } from '@angular/core';
     </div>
     <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+            <li *ngFor="let hero of heroes"
+             [class.selected]="hero === selectedHero"
+             (click)="onSelect(hero)">
                <span class="badge">{{hero.id}}</span> {{hero.name}}
              </li>
         </ul>
@@ -71,30 +92,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'Tour of Heroes';
-    selectedHero: Hero;
+    selectedHero:Hero;
     heroes = HEROES;
-    onSelect(hero: Hero): void {
+
+    onSelect(hero:Hero):void {
         this.selectedHero = hero;
     }
 }
 
 
-export class Hero {
-    id:number;
-    name:string;
-}
 
-const HEROES:Hero[] = [
-    {id: 11, name: 'Mr. Nice'},
-    {id: 12, name: 'Narco'},
-    {id: 13, name: 'Bombasto'},
-    {id: 14, name: 'Celeritas'},
-    {id: 15, name: 'Magneta'},
-    {id: 16, name: 'RubberMan'},
-    {id: 17, name: 'Dynama'},
-    {id: 18, name: 'Dr IQ'},
-    {id: 19, name: 'Magma'},
-    {id: 20, name: 'Tornado'}
-];
 
 
