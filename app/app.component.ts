557b8cda268a342f256682362093fb51acefc862
Nithin Ankam <c.nithin.ankam@uptake.com>
2016-09-21 23:30:45 -0500

    add a click handler and *ngIf to show only selected hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,15 +3,17 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
+    <div *ngIf="selectedHero">
+       <h2>{{selectedHero.name}} details!</h2>
+        <div><label>id: </label>{{selectedHero.id}}</div>
+        <div>
+         <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+        </div>
     </div>
     <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="let hero of heroes">
+            <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
                <span class="badge">{{hero.id}}</span> {{hero.name}}
              </li>
         </ul>
@@ -69,11 +71,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero:Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    selectedHero: Hero;
     heroes = HEROES;
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
 
 
@@ -94,3 +96,5 @@ const HEROES:Hero[] = [
     {id: 19, name: 'Magma'},
     {id: 20, name: 'Tornado'}
 ];
+
+
